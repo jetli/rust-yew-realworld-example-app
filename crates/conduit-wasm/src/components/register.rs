28ad1a90178e46c175f067c315e18ad17d367117
Jet Li <jing.i.qin@icloud.com>
2019-11-28 15:48:19 +0800

    fix typo

--- crates/conduit-wasm/src/components/register.rs
+++ crates/conduit-wasm/src/components/register.rs
@@ -23,7 +23,7 @@ impl Component for Register {
                 <div class="container page">
                     <div class="row">
                         <div class="col-md-6 offset-md-3 col-xs-12">
-                            <h1 class="text-xs-center">{ "Sign In" }</h1>
+                            <h1 class="text-xs-center">{ "Sign Up" }</h1>
                             <p class="text-xs-center">
                                 <RouterLink text="Have an account?" link="/#/login"/>
                             </p>
