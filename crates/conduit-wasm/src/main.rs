bbe010055604699a7236f2da425f297012178c65
Jet Li <jing.i.qin@icloud.com>
2019-12-16 01:38:04 +0800

    optimize performance and .wasm file size

--- crates/conduit-wasm/src/main.rs
+++ crates/conduit-wasm/src/main.rs
@@ -2,6 +2,10 @@ use web_logger;
 
 use conduit_wasm::App;
 
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 fn main() {
     web_logger::init();
     yew::start_app::<App>();
